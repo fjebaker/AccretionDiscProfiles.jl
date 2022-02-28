d49931e84848cee607fff5589c7203ae882b8154
fjebaker <fergusbkr@gmail.com>
2022-02-28 17:20:18 +0000

    bug fixes and working samplers

--- src/corona-models.jl
+++ src/corona-models.jl
@@ -6,18 +6,28 @@
     ϕ = 0.0
 end
 
-function sample_position(::AbstractMetricParams{T}, model::LampPostModel{T}, us, N) where {T}
+function sample_position(::AbstractMetricParams{T}, model::LampPostModel{T}, N) where {T}
     u = @SVector [T(0.0), model.h, model.θ, model.ϕ]
-    fill(u, n_samples)
+    fill(u, N)
 end
 
-function sample_even_direction(m::AbstractMetricParams{T}, ::LampPostModel{T}, us, N) where {T}
-    map(range(N)) do index
-        # uniform angle
-        i = index / N
-        ϕ = i * 2π
-        θ = acos(1 - 2i)
-        r, t, p = vector_to_local_sky(m, @view(us[i]), θ, ϕ)
+function sample_random_direction(m::AbstractMetricParams{T}, ::LampPostModel{T}, us, N) where {T}
+    map(1:N) do index
+        ϕ = rand(T) * 2π
+        θ = acos(1 - 2*rand(T))
+        r, t, p = vector_to_local_sky(m, us[index], θ, ϕ)
         @SVector [T(0.0), r, t, p]
     end
 end
+
+function sample_even_direction(m::AbstractMetricParams{T}, ::LampPostModel{T}, us, N) where {T}
+    map(1:N) do index
+        θ = acos(1 - 2 * (index / N))
+        # golden ratio
+        ϕ = π * (1 + √5) * index
+        r, t, p = vector_to_local_sky(m, us[index], θ, ϕ)
+        @SVector [T(0.0), r, t, p]
+    end
+end
+
+export LampPostModel
