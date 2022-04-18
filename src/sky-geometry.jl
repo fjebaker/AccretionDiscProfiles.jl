4f27e5f0aacca07df196c7c045159abc422dd9a1
fjebaker <fergusbkr@gmail.com>
2022-04-18 16:23:29 +0100

    reworked sampling api and added uniform sampler

--- src/sky-geometry.jl
+++ src/sky-geometry.jl
@@ -1,3 +1,51 @@
+abstract type AbstractSkyDomain end
+
+struct LowerHemisphere <: AbstractSkyDomain end
+struct BothHemispheres <: AbstractSkyDomain end
+
+abstract type AbstractDirectionSampler{AbstractSkyDomain} end
+
+struct RandomSampler{D} <: AbstractDirectionSampler{D} 
+    RandomSampler(domain::AbstractSkyDomain) = new{domain}()
+    RandomSampler(;domain::AbstractSkyDomain=LowerHemisphere()) = new{typeof(domain)}()
+end
+struct EvenSampler{D} <: AbstractDirectionSampler{D} 
+    EvenSampler(domain::AbstractSkyDomain) = new{domain}()
+    EvenSampler(;domain::AbstractSkyDomain=LowerHemisphere()) = new{typeof(domain)}()
+end
+struct UniformProjectionSampler{D} <: AbstractDirectionSampler{D}
+    resolution::Float64
+    UniformProjectionSampler(res, domain::AbstractSkyDomain) = new{typeof(domain)}(res)
+    UniformProjectionSampler(;res=100.0, domain::AbstractSkyDomain=LowerHemisphere()) = UniformProjectionSampler(res, domain)
+end
+
+sample_azimuth(sm::AbstractDirectionSampler{D}, i) where {D} = error("Not implemented for $(typeof(sm)).")
+sample_elevation(sm::AbstractDirectionSampler{D}, i) where {D} = error("Not implemented for $(typeof(sm)).")
+sample_angles(sm::AbstractDirectionSampler{D}, i, N) where {D} = (sample_elevation(sm, i/N), sample_azimuth(sm, i))
+
+# even
+sample_azimuth(::RandomSampler{D}, i) where {D} = rand(Float64) * 2π
+sample_elevation(::RandomSampler{LowerHemisphere}, i) = acos(1 - rand(Float64))
+sample_elevation(::RandomSampler{BothHemispheres}, i) = acos(1 - 2*rand(Float64))
+
+# even
+sample_azimuth(::EvenSampler{D}, i) where {D} = π * (1 + √5) * i
+sample_elevation(::EvenSampler{LowerHemisphere}, i) = acos(1 - i)
+sample_elevation(::EvenSampler{BothHemispheres}, i) = acos(1 - 2i)
+
+# uniform
+sample_azimuth(::UniformProjectionSampler{D}, i) where {D} = π * (1 + √5) * i
+sample_elevation(sm::UniformProjectionSampler{LowerHemisphere}, i) = π - 2atan(√(sm.resolution/i))
+function sample_elevation(sm::UniformProjectionSampler{BothHemispheres}, i)
+    ϕ = 2atan(√(sm.resolution/i))
+    if iseven(i)
+        ϕ
+    else
+        π - ϕ
+    end
+end
+sample_angles(sm::UniformProjectionSampler{D}, i, N) where {D} = (sample_elevation(sm, i), sample_azimuth(sm, i))
+
 
 function convert_angles(a, r, θ, ϕ, θ_obs, ϕ_obs)
     ϕ̃ = ϕ - ϕ_obs
