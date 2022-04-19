ae56d5110cda1d5f542107103e0ebbb70bcac610
Fergus Baker <fergusbkr@gmail.com>
2022-04-19 23:55:26 +0100

    Uniform Sampling Methods (#6)
    * reworked sampling api and added uniform sampler
    
    * bump version
    
    * added geometrical predicates
    
    * rename uniform sampler to Weierstrass
    
    * julia formatting
    
    * added missing file
    
    * removed file not relevant to this uniform sampling

--- src/AccretionDiscProfiles.jl
+++ src/AccretionDiscProfiles.jl
@@ -12,50 +12,21 @@ import GeodesicBase: AbstractMetricParams
 import GeodesicTracer: tracegeodesics
 import AccretionGeometry: AbstractAccretionGeometry, tracegeodesics
 
+import GeometricalPredicates: Point2D, getx, gety, geta, getb, getc
 
-abstract type AbstractCoronaModel{T} end
-
-sample_position(m::AbstractMetricParams{T}, model::AbstractCoronaModel{T}, N) where {T} = error("Not implemented for $(typeof(model)).")
-
-function sample_random_direction(m::AbstractMetricParams{T}, model::AbstractCoronaModel{T}, us, N) where {T}
-    map(1:N) do index
-        ϕ = rand(T) * 2π
-        θ = acos(1 - 2*rand(T))
-        r, t, p = vector_to_local_sky(m, us[index], θ, ϕ)
-        @SVector [T(0.0), r, t, p]
-    end
-end
-
-function sample_even_direction(m::AbstractMetricParams{T}, model::AbstractCoronaModel{T}, us, N) where {T}
-    map(1:N) do index
-        θ = acos(1 - 2 * (index / N))
-        # golden ratio
-        ϕ = π * (1 + √5) * index
-        r, t, p = vector_to_local_sky(m, us[index], θ, ϕ)
-        @SVector [T(0.0), r, t, p]
-    end
-end
-
-@inline function sample_velocity(m::AbstractMetricParams{T}, model::AbstractCoronaModel{T}, us, N; sampler) where {T}
-    if sampler == :random
-        sample_random_direction(m, model, us, N)
-    elseif sampler == :even
-        sample_even_direction(m, model, us, N)
-    else
-        error("Unknown sampling method $(sampler)")
-    end
-end
+include("sky-geometry.jl")
+include("corona-models.jl")
 
 function tracegeodesics(
     m::AbstractMetricParams{T},
     model::AbstractCoronaModel{T},
     time_domain::Tuple{T,T};
-    n_samples=1024,
-    sampler=:even,
-    kwargs...
+    n_samples = 1024,
+    sampler = WeierstrassSampler(res = 100.0),
+    kwargs...,
 ) where {T}
     us = sample_position(m, model, n_samples)
-    vs = sample_velocity(m, model, us, n_samples; sampler=sampler)
+    vs = sample_velocity(m, model, sampler, us, n_samples)
     tracegeodesics(m, us, vs, time_domain; kwargs...)
 end
 
@@ -66,12 +37,12 @@ function tracegeodesics(
     time_domain::Tuple{T,T},
     d::AbstractAccretionGeometry{T},
     ;
-    n_samples=1024,
-    sampler=:even,
-    kwargs...
+    n_samples = 1024,
+    sampler = WeierstrassSampler(res = 100.0),
+    kwargs...,
 ) where {T}
     us = sample_position(m, model, n_samples)
-    vs = sample_velocity(m, model, us, n_samples; sampler=sampler)
+    vs = sample_velocity(m, model, sampler, us, n_samples)
     tracegeodesics(m, us, vs, time_domain, d; kwargs...)
 end
 
@@ -80,18 +51,13 @@ function renderprofile(
     m::AbstractMetricParams{T},
     model::AbstractCoronaModel{T},
     max_time::T,
-    d::AbstractAccretionGeometry{T}
-    ;
-    n_samples=2048,
-    sampler=:even,
-    kwargs...
+    d::AbstractAccretionGeometry{T};
+    n_samples = 2048,
+    sampler = WeierstrassSampler(res = 100.0),
+    kwargs...,
 ) where {T}
     __renderprofile(m, model, d, n_samples, (0.0, max_time); kwargs...)
 end
-
-
-include("sky-geometry.jl")
-include("corona-models.jl")
 
 export AbstractCoronaModel, tracegeodesics, renderprofile
 
--- src/sky-geometry.jl
+++ src/sky-geometry.jl
@@ -1,3 +1,57 @@
+abstract type AbstractSkyDomain end
+
+struct LowerHemisphere <: AbstractSkyDomain end
+struct BothHemispheres <: AbstractSkyDomain end
+
+abstract type AbstractDirectionSampler{AbstractSkyDomain} end
+
+struct RandomSampler{D} <: AbstractDirectionSampler{D}
+    RandomSampler(domain::AbstractSkyDomain) = new{domain}()
+    RandomSampler(; domain::AbstractSkyDomain = LowerHemisphere()) = new{typeof(domain)}()
+end
+struct EvenSampler{D} <: AbstractDirectionSampler{D}
+    EvenSampler(domain::AbstractSkyDomain) = new{domain}()
+    EvenSampler(; domain::AbstractSkyDomain = LowerHemisphere()) = new{typeof(domain)}()
+end
+struct WeierstrassSampler{D} <: AbstractDirectionSampler{D}
+    resolution::Float64
+    WeierstrassSampler(res, domain::AbstractSkyDomain) = new{typeof(domain)}(res)
+    WeierstrassSampler(; res = 100.0, domain::AbstractSkyDomain = LowerHemisphere()) =
+        WeierstrassSampler(res, domain)
+end
+
+sample_azimuth(sm::AbstractDirectionSampler{D}, i) where {D} =
+    error("Not implemented for $(typeof(sm)).")
+sample_elevation(sm::AbstractDirectionSampler{D}, i) where {D} =
+    error("Not implemented for $(typeof(sm)).")
+sample_angles(sm::AbstractDirectionSampler{D}, i, N) where {D} =
+    (sample_elevation(sm, i / N), sample_azimuth(sm, i))
+
+# even
+sample_azimuth(::RandomSampler{D}, i) where {D} = rand(Float64) * 2π
+sample_elevation(::RandomSampler{LowerHemisphere}, i) = acos(1 - rand(Float64))
+sample_elevation(::RandomSampler{BothHemispheres}, i) = acos(1 - 2 * rand(Float64))
+
+# even
+sample_azimuth(::EvenSampler{D}, i) where {D} = π * (1 + √5) * i
+sample_elevation(::EvenSampler{LowerHemisphere}, i) = acos(1 - i)
+sample_elevation(::EvenSampler{BothHemispheres}, i) = acos(1 - 2i)
+
+# uniform
+sample_azimuth(::WeierstrassSampler{D}, i) where {D} = π * (1 + √5) * i
+sample_elevation(sm::WeierstrassSampler{LowerHemisphere}, i) =
+    π - 2atan(√(sm.resolution / i))
+function sample_elevation(sm::WeierstrassSampler{BothHemispheres}, i)
+    ϕ = 2atan(√(sm.resolution / i))
+    if iseven(i)
+        ϕ
+    else
+        π - ϕ
+    end
+end
+sample_angles(sm::WeierstrassSampler{D}, i, N) where {D} =
+    (sample_elevation(sm, i), sample_azimuth(sm, i))
+
 
 function convert_angles(a, r, θ, ϕ, θ_obs, ϕ_obs)
     ϕ̃ = ϕ - ϕ_obs
@@ -16,3 +70,5 @@ end
 function vector_to_local_sky(m::BoyerLindquist{T}, u, θ, ϕ) where {T}
     convert_angles(m.a, u[2], u[3], u[4], θ, ϕ)
 end
+
+export LowerHemisphere, BothHemispheres, RandomSampler, EvenSampler, WeierstrassSampler
